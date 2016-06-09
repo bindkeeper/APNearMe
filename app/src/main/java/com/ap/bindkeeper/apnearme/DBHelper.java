c115db5fdbe69f7378ab421524778ba4cd0e2775
bindkeeper <bindkeeper@gmail.com>
2016-06-09 09:38:59 +0300

    added new table and getSearch method

--- app/src/main/java/com/ap/bindkeeper/apnearme/DBHelper.java
+++ app/src/main/java/com/ap/bindkeeper/apnearme/DBHelper.java
@@ -18,6 +18,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String nameRow = "name";
     private static final String addressRow = "address";
     private static final String table = "favourites";
+    private static final String tableSearch = "searchTable";
 
     public DBHelper(Context context) {
         super(context, filename, null, version);
@@ -26,7 +27,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE " + table + " ("+ nameRow +" TEXT, "+ addressRow +" TEXT )");
-
+        db.execSQL("CREATE TABLE " + tableSearch + " ("+ nameRow +" TEXT, "+ addressRow +" TEXT )");
     }
 
     @Override
@@ -51,6 +52,18 @@ public class DBHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor res =  db.rawQuery( "select * from "+table, null );
+
+        while (res.moveToNext()) {
+            favorites.add(new Place(res.getString(0), res.getString(1)));
+        }
+        return favorites;
+    }
+
+    public ArrayList<Place> getSearch () {
+        ArrayList<Place> favorites = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor res =  db.rawQuery( "select * from "+tableSearch, null );
 
         while (res.moveToNext()) {
             favorites.add(new Place(res.getString(0), res.getString(1)));
